c3d7101621b4269820084ecaecdc9614c4b79bc1
1612224 <1612224@hcmus.edu.vn>
2019-06-30 13:41:40 +0700

    fix seeders

--- seeders/20190627080227-demo-thanh-vien.js
+++ seeders/20190627080227-demo-thanh-vien.js
@@ -12,24 +12,24 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
-   return queryInterface.bulkInsert('thanh_viens', [
-    {ma_thanh_vien:2,diem_ca_nhan:20,
-      created_at:new Date(),
-      updated_at:new Date(),
-      tai_khoan_ma_tai_khoan:2
-    },
-    {ma_thanh_vien:3,diem_ca_nhan:20,
-      created_at:new Date(),
-      updated_at:new Date(),
-      tai_khoan_ma_tai_khoan:3
-    },
-    {ma_thanh_vien:4,diem_ca_nhan:20,
-      created_at:new Date(),
-      updated_at:new Date(),
-      tai_khoan_ma_tai_khoan:4
-    }
-   ], {});
-   
+    return queryInterface.bulkInsert('thanh_viens', [
+      {
+        ma_thanh_vien: 2, diem_ca_nhan: 20,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        ma_thanh_vien: 3, diem_ca_nhan: 20,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        ma_thanh_vien: 4, diem_ca_nhan: 20,
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+    ], {});
+
   },
 
   down: (queryInterface, Sequelize) => {
