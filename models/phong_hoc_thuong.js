70bc66872955e60fce6395bd392d7d672fb20311
1612224 <1612224@hcmus.edu.vn>
2019-06-30 13:44:53 +0700

    merge conflict

--- models/phong_hoc_thuong.js
+++ models/phong_hoc_thuong.js
@@ -11,13 +11,8 @@ module.exports = (sequelize, DataTypes) => {
   }, { underscored: true });
   phong_hoc_thuong.associate = function (models) {
     // associations can be defined here
-<<<<<<< HEAD
-    phong_hoc_thuong.belongsTo(models.toa_nha,{foreignKey:'thuoc_toa_nha',targetKey:'ma_toa_nha'});
-    phong_hoc_thuong.belongsTo(models.phong,{foreignKey:'ma_phong_hoc_thuong',targetKey:'ma_phong'});
-=======
     phong_hoc_thuong.belongsTo(models.toa_nha, { foreignKey: 'thuoc_toa_nha', targetKey: 'ma_toa_nha' });
     phong_hoc_thuong.belongsTo(models.phong, { foreignKey: 'ma_phong_hoc_thuong', targetKey: 'ma_phong' });
->>>>>>> 2f32e47724992c7f559f52d7c2271a3adaf9e749
   };
   return phong_hoc_thuong;
 };
