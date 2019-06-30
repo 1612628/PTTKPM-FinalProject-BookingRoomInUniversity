33caa69b94d469a8cfdadd22ac6ad3b2e520e626
Duy-Thanh Nguyen <thanhnguyenduy2304@gmail.com>
2019-06-30 13:46:44 +0700

    nothing

--- models/thanh_vien.js
+++ models/thanh_vien.js
@@ -10,13 +10,8 @@ module.exports = (sequelize, DataTypes) => {
   }, { underscored: true });
   thanh_vien.associate = function (models) {
     // associations can be defined here
-<<<<<<< HEAD
-    thanh_vien.hasMany(models.chi_tiet_dat_phong,{foreignKey:'thanh_vien_dat',sourceKey:'ma_thanh_vien'});
-    thanh_vien.belongsTo(models.tai_khoan,{foreignKey:'ma_thanh_vien',targetKey:'ma_tai_khoan'});
-=======
     thanh_vien.hasMany(models.chi_tiet_dat_phong, { foreignKey: 'thanh_vien_dat', sourceKey: 'ma_thanh_vien' });
     thanh_vien.belongsTo(models.tai_khoan, { foreignKey: 'ma_thanh_vien', targetKey: 'ma_tai_khoan' });
->>>>>>> 2f32e47724992c7f559f52d7c2271a3adaf9e749
   };
   return thanh_vien;
 };
